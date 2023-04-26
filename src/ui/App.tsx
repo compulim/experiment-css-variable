8a5474a9041c73e43b95f97924749e1be6944c60
William Wong <compulim@users.noreply.github.com>
2023-04-26 22:14:17 +0000

    List limitations

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,8 +4,8 @@ var App = () => {
   var backgroundColor = useMemo(() => getComputedStyle(document.body).getPropertyValue('background-color'), []);
   var variableOne = useMemo(() => getComputedStyle(document.body).getPropertyValue('--var-one'), []);
   var variableTwo = useMemo(() => getComputedStyle(document.body).getPropertyValue('--var-two'), []);
-  var variableThree = useMemo(() => (getComputedStyle(document.body) as any)['-ie-var-three'], []);
-  var variableFour = useMemo(() => (getComputedStyle(document.body) as any)['-ie-var-four'], []);
+  var variableThree = useMemo(() => (getComputedStyle(document.body) as any)['-var-three'], []);
+  var variableFour = useMemo(() => (getComputedStyle(document.body) as any)['-var-four'], []);
 
   // Check mutation.
 
@@ -17,33 +17,55 @@ var App = () => {
           <code>background-color</code>
         </dt>
         <dd>
-          {backgroundColor} (type of <code>{typeof backgroundColor}</code>)
+          {JSON.stringify(backgroundColor)} <small>(type of <code>{typeof backgroundColor}</code>)</small>
         </dd>
         <dt>
           <code>--var-one</code>
         </dt>
         <dd>
-          {variableOne} (type of <code>{typeof variableOne}</code>)
+          {JSON.stringify(variableOne)} <small>(type of <code>{typeof variableOne}</code>)</small>
         </dd>
         <dt>
           <code>--var-two</code>
         </dt>
         <dd>
-          {variableTwo} (type of <code>{typeof variableTwo}</code>)
+          {JSON.stringify(variableTwo)} <small>(type of <code>{typeof variableTwo}</code>)</small>
         </dd>
         <dt>
-          <code>-ie-var-three</code>
+          <code>-var-three</code>
         </dt>
         <dd>
-          {variableThree} (type of <code>{typeof variableThree}</code>)
+          {JSON.stringify(variableThree)} <small>(type of <code>{typeof variableThree}</code>)</small>
         </dd>
         <dt>
-          <code>-ie-var-four</code>
+          <code>-var-four</code>
         </dt>
         <dd>
-          {variableFour} (type of <code>{typeof variableFour}</code>)
+          {JSON.stringify(variableFour)} <small>(type of <code>{typeof variableFour}</code>)</small>
         </dd>
       </dl>
+      <p>IE Mode has some limitations:</p>
+      <ul>
+        <li>
+          Only one dash `-` is allowed
+          <ul>
+            <li>However, one dash is not supported by modern browsers</li>
+          </ul>
+        </li>
+        <li>
+          Use indexer of <code>CSSStyleDeclaration</code>, instead of{' '}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue">
+            <code>getPropertyValue</code>
+          </a>
+          <ul>
+            <li>
+              For example, <code>getComputedStyle(element)['-my-var']</code>
+            </li>
+          </ul>
+        </li>
+        <li>Need to check: CSS cascading</li>
+        <li>Need to check: MutationObserver</li>
+      </ul>
     </div>
   );
 };
